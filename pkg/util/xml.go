bbe5a130714f8ac367d75d4e1eb597b24a7d9eb0
unknown <panzm@trump.local>
2019-06-27 12:34:31 +0800

    init

--- pkg/util/xml.go
+++ pkg/util/xml.go
@@ -6,19 +6,63 @@ import (
 	"os"
 	"path/filepath"
 	"fmt"
-	"io/ioutil"
+	"github.com/beevik/etree"
 )
 
-func ReadSource(src, pwd string) ([]byte, error) {
+func ReadSource(env string) (map[string]interface{}, error) {
 
-	abspath := filepath.Join(src, pwd)
-	file, err := os.Open(abspath)
-	defer file.Close()
-	
+	pwd, err := os.Getwd()
 	if err != nil {
-		fmt.Println("文件读取错误")
+		fmt.Println(err)
+	}
+	
+	src := "env_info.xml"
+	abspath := filepath.Join(pwd, src)
+	
+	doc := etree.NewDocument()
+	if err := doc.ReadFromFile(abspath); err != nil {
+		fmt.Printf("文件读取失败，请检查文件%s是否存在", abspath)
 	}
 
-	fmt.Println(abspath)
-	return ioutil.ReadAll(file)
+	root := doc.SelectElement("environment")
+	// 使用interface{}初始化一个一维映射，interface{} 可以代表任意类型
+	// interface{} 就是一个空接口，所有类型都实现了这个接口，所以它可以代表所有类型
+	env_info := make(map[string]interface{})
+	
+	for _, info := range root.SelectElements(env) {
+		arr := [4]string{"mysql", "redis", "ssh", "ftp"}
+		for _, value := range arr {
+			env_info[value] = readTypeValue(info, value)
+		}
+	}
+	
+	return env_info, err
 }
+
+
+func readTypeValue(info *etree.Element, choose_type string) (map[string]interface{}) {
+	result := make(map[string]interface{})
+	type_choice := "./" + choose_type + "/*"
+
+	for _, e := range info.FindElements(type_choice) {
+		result[e.Tag] = e.Text()
+	}
+
+	return result
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routers/api/v1/env.go
+++ routers/api/v1/env.go
@@ -1,38 +1,28 @@
 package v1
 
 import (
-	"github.com/gin-gonic/gin"
-    "os"
-    "path/filepath"
+
+    "github.com/gin-gonic/gin"
+    "goodliving/rf-env/pkg/util"
     "fmt"
-    "github.com/beevik/etree"
+    "net/http"
+
 )
 
 func GetEnv(c *gin.Context) {
     
-    pwd, err := os.Getwd()
+    env := c.Param("env")
+
+    data, err := util.ReadSource(env)
     if err != nil {
         fmt.Println(err)
     }
 
-    env := c.Param("env")
-
-    filename := "env_info.xml"
-
-    abspath := filepath.Join(pwd, filename)
-
-    doc := etree.NewDocument()
-    if err := doc.ReadFromFile(abspath); err != nil {
-        fmt.Printf("文件读取失败，请检查文件%s是否存在", abspath)
-    }
-
-    //读取文件内容
-    root := doc.SelectElement("environment")
-    fmt.Println("ROOT element:", root.Tag)
-
-    for _, info := range root.SelectElements(env) {
-        fmt.Printf("查询环境为%s", env)
-        fmt.Println(info)
-    }
+    c.JSON(http.StatusOK, gin.H{
+        "code" : "1000",
+        "msg" : "处理成功",
+        "env" : env,
+        "data" : data,
+    })
 
 }
